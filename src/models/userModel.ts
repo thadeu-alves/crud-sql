2b6e476d52a8de1ca815a9022da8891e371e4d83
thadeu-alves <thadeutelecentrosg@gmail.com>
2025-09-12 12:28:19 -0300

    create user model

--- src/models/userModel.ts
+++ src/models/userModel.ts
@@ -6,3 +6,31 @@ export interface IUserModel {
     createdAt: Date;
     updatedAt: Date;
 }
+
+export class UserModel {
+    private props: IUserModel;
+
+    constructor(props: IUserModel) {
+        this.props = props;
+    }
+
+    getName() {
+        return this.props.name;
+    }
+
+    getId() {
+        return this.props.id;
+    }
+
+    getEmail() {
+        return this.props.email;
+    }
+
+    getAge() {
+        return this.props.age;
+    }
+
+    getProps() {
+        return this.props;
+    }
+}
